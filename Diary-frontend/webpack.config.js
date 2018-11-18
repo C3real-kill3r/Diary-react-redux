fc0bf2c4bf5569f6d9a8519472cfbe82db4bb924
brian okuku <brybzi@gmail.com>
2018-11-18 16:41:17 +0300

    chore(CSS): add CSS configuration
    - sets up css
    
    [Maintains #162036466]

--- Diary-frontend/webpack.config.js
+++ Diary-frontend/webpack.config.js
@@ -1,6 +1,6 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-const htmlPlugin = new HtmlWebPackPlugin({
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
   template: "./src/index.html",
   filename: "./index.html"
 });
@@ -14,8 +14,26 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
       }
     ]
   },
-  plugins: [htmlPlugin]
+  plugins: [htmlWebpackPlugin]
 };
